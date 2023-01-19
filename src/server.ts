f368d588eb2a75b7bcb255ba9925b01bdf6b0254
opatricpires <patric.pires@outlook.com>
2023-01-19 00:26:41 -0300

    style: change project structure

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
 import Fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes";
 import cors from "@fastify/cors";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get("/hello", async () => {
-  const habits = await prisma.habit.findMany();
-
-  return habits;
-});
+app.register(appRoutes);
 
 app.listen({
   port: 3333,
